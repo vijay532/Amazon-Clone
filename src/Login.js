4a3e436c5197670a2668efb50560be8279de6126
vijay <sahaniv66@gmail.com>
2020-09-18 14:01:47 +0530

    login and regiter using firebase auth

--- src/Login.js
+++ src/Login.js
@@ -1,7 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.css'
-import {Link} from 'react-router-dom'
+import {Link, useHistory} from 'react-router-dom'
+import { auth } from './firebase';
 function Login() {
+    const [email,setEmail]=useState('');        //keep track whatever u type in email
+    const [password,setPassword]=useState('');
+    const history=useHistory();     // allows us programmatically change the url
+
+    const signIn=e=>{
+        e.preventDefault();
+
+        //some fancy firebase login
+
+        auth.signInWithEmailAndPassword(email, password)
+            .then(auth => {
+                history.push('/')
+            })
+            .catch(error => alert(error.message))
+
+    }
+
+    const register=e=>{
+
+        e.preventDefault();  //for avoid loss of data after refreshing 
+
+        //some fancy firebase register
+
+        auth.createUserWithEmailAndPassword(email,password)
+        .then((auth)=>{
+            if (auth) {
+                history.push('/');
+            }
+            console.log(auth);
+        }).catch(error=>
+            alert(error.message))
+    }
     return (
         <div className="login">
             <Link to="/">
@@ -11,14 +44,15 @@ function Login() {
                 <h1 className="login__sign">Sign-in</h1>
                 <form>
                     <h5>E-mail</h5>
-                    <input type="text"/>
+                    <input type="text" value={email} onChange=
+                    {e=>setEmail(e.target.value)}/>
                     <h5>Password</h5>
-                    <input type="password"/>
-                    <button className="login__signInButton">Sign In</button>
+                    <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+                    <button type="submit" onClick={signIn} className="login__signInButton">Sign In</button>
                 </form>
                 <p>By Signing-in you agree to the AMAZON FAKE CLONE terms and conditions
                 </p>
-                <button className="login__registerButton">Create your Amazon Account</button>
+                <button onClick={register}className="login__registerButton">Create your Amazon Account</button>
             </div>
             
         </div>
