8bf4f8091f37942260300f8379b8ec00ce5f774f
vijay <sahaniv66@gmail.com>
2020-09-18 14:01:01 +0530

    user signIn and SignOut event dispatcher added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,48 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Header from './Header';
 import Home from './Home';
+import Login from './Login';
 import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
 import Checkout from './Checkout'
+import { auth } from "./firebase";
+import { useStateValue } from './StateProvider';
+
 function App() {
+
+  const [{}, dispatch] = useStateValue();
+
+  useEffect(() => {
+    // will only run once when the app component loads...
+
+    auth.onAuthStateChanged((authUser) => {
+      console.log("THE USER IS >>> ", authUser);
+
+      if (authUser) {
+        // the user just logged in / the user was logged in
+
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        // the user is logged out
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
+
+
   return (
+
     <Router>
       <div className="App">
         <Switch>
           <Route path="/login">
-            <p>this is login page </p>
+            <Login/>
           </Route>
           <Route path="/checkout">
             <Header/>
