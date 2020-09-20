357c1a359c9b5e7ecb309ff8e63371ded02d5c00
vijay <sahaniv66@gmail.com>
2020-09-20 14:00:27 +0530

    payment page added

---
+++ src/Payment.js
@@ -0,0 +1,100 @@
+import React, { useState }from 'react'
+import Checkout from './Checkout';
+import './Payment.css'
+import { useStateValue } from './StateProvider'
+import CheckoutProduct from './CheckoutProduct'
+import { Link } from "react-router-dom";
+import CurrencyFormat from 'react-currency-format'; 
+import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
+import {calculateItems} from './reducer';
+function Payment() {
+    const [{basket,user},dispatch]=useStateValue();
+
+    const stripe=useStripe();
+    const element=useElements();
+
+    const [succeeded, setSucceeded] = useState(false);
+    const [processing, setProcessing] = useState("");
+    
+    const [error, setError] = useState(null);
+    const [disabled, setDisabled] = useState(true);
+
+    const handleSubmit =e=>{
+        // do all fancy stripe stuff.....
+    }
+
+    const handleChange=event=>{
+        // Listen for changes in the CardElement
+        // and display any errors as the customer types their card details
+        setDisabled(event.empty);
+        setError(event.error ? event.error.message : "");
+    }
+
+    return (
+        <div className="payment">
+            <div className="payment__container">
+                <h1>Checkout (
+                <Link to="/checkout">{basket?.length} items</Link>)
+                </h1>
+                <div className="payment__section">
+                    <div className="payment__title">
+                        <h3>Delivery Address</h3>
+                    </div>
+                    <div className="payment__address">
+                        <p>Hello, {user?.email}</p>
+                        <p>A-104,Thane</p>
+                    </div>
+
+                </div>
+                <div className="payment__section">
+                    <div className="payment__title">
+                        <h3>Review items and delivery</h3>
+                    </div>
+                    <div className="payment__items">
+                        {basket.map(item=>(
+                                <CheckoutProduct
+                                id={item.id}
+                                title={item.title}
+                                image={item.image}
+                                price={item.price}
+                                rating={item.rating}
+                                />
+                            ))}
+                    </div>
+                </div>
+                <div className="payment__section">
+                    <div className="payment__method">
+                        <h3>Payment Method</h3>
+                    </div>
+                    <div className="payment__details">
+                        {/*stripe will go here */}
+                        <form onSubmit={handleSubmit}>
+                            <CardElement onChange={handleChange}/>
+                            <div className="payment__priceContainer">
+                            <CurrencyFormat
+                                renderText={(value) => (
+                                    <h3>Order Total: {value}</h3>
+                                )}
+                                decimalScale={2}
+                                value={calculateItems(basket)}
+                                displayType={"text"}
+                                thousandSeparator={true}
+                                prefix={"$"}
+                            />
+                            <button disabled={processing || disabled || succeeded}>
+                                <span>{processing ? <p>Processing</p> : "Buy Now"}</span>
+                            </button>
+
+                            </div>
+                            {error && <div>{error}</div>}
+                        </form>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        
+    )
+}
+
+export default Payment
