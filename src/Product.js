1b27ebe35076f8c788606d8f858bffa2eacf07f8
vijay <sahaniv66@gmail.com>
2020-09-25 20:18:06 +0530

    Product added

--- src/Product.js
+++ src/Product.js
@@ -23,7 +23,7 @@ function Product({id,title,price,rating,image}) {
             <div className="product__info">
                 <p>{title}</p>
                 <p classname="product__price">
-                    <small>$</small>
+                    <small>₹</small>
                     <strong>{price}</strong>
                 </p>
                 <div className="product__rating">
